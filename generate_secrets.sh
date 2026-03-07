eb43ae5b7704b4d76506d2c55fa424dcf0ac0e9f
Pablo <pablo.esteban.sanchez@gmail.com>
2026-03-07 19:49:39 +0100

    Añadido servicio ftp y smb

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -14,5 +14,16 @@ printf "BANCO_PIN=%04d\n" $PIN > env/banco.env
 DBZ_PASS=$(shuf -n 1 diccionario_seguro.txt)
 echo "DBZ_PASS=$DBZ_PASS" > env/dbz.env
 
+# 3. Elegimos una contraseña aleatoria del diccionario de servicios
+PASS_FTP_usuario1=$(shuf -n 1 diccionario_servicios.txt)
+PASS_FTP_usuario2=$(shuf -n 1 diccionario_servicios.txt)
+PASS_SMB_usuario1=$(shuf -n 1 diccionario_servicios.txt)
+PASS_SMB_usuario2=$(shuf -n 1 diccionario_servicios.txt)
+# Guardamos en .env
+echo "FTP_USERS=bisbal|$PASS_FTP_usuario1|/home/bisbal admin|$PASS_FTP_usuario1|/home/admin" > env/ftp.env
+echo "SMB_PASS_usuario1=$PASS_SMB_usuario1" > env/samba.env
+echo "SMB_PASS_usuario2=$PASS_SMB_usuario2" >> env/samba.env
+
+
 # Dar permisos restrictivos para que no se puedan leer fácilmente si no eres root
 chmod 600 env/*.env
